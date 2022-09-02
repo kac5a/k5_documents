85da4d6575860fb9db8c9bdc0f06913c79a00fd7
kac5a <kacsandi.martin@enet.hu>
2022-09-02 23:31:40 +0200

    Add SQL file

---
+++ k5documents.sql
@@ -0,0 +1,16 @@
+USE `es_extended`;
+
+CREATE TABLE `k5_documents` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` longtext,
+  `ownerId` varchar(50),
+  `isCopy` tinyint,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `k5_document_templates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` longtext,
+  `job` varchar(50),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
