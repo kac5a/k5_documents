bb7c43865a0132236759236f17b2bef9939cddf0
kac5a <kacsandi.martin@enet.hu>
2022-09-12 09:11:21 +0200

    fix fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,8 @@ fx_version "cerulean"
 
 description "A better document management script"
 author "K5 Scripts"
-<<<<<<< HEAD
-version '1.1.2'
-update "Design changes, added version check"
-=======
 version '1.1.3'
 update "Added option to create documents from other scripts"
->>>>>>> e5cfc1b (add server document, better version check)
 repository 'https://github.com/kac5a/k5_documents'
 
 lua54 'yes'
